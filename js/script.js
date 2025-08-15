6a89eeb38f14eb7d34acb61b65bbf042afbcc7e5
Carlos Eduardo Gonçalves de Lima Duarte <kdwduarte@gmail.com>
2025-08-14 22:29:28 -0300

    Add blog page and enhance homepage UI/UX
    Introduced a new blog.html page and updated index.html with new sections for slider and blog, improved navigation, and added AOS and Font Awesome dependencies. Enhanced style.css with new styles for navbar, slider, and blog elements. Updated script.js to support AOS animations, navbar scroll effect, and interactive slider images.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
+AOS.init();
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+
 const cards = document.querySelectorAll('.carta');
 
 cards.forEach(card => {
   const img = card.querySelector('#cardapio img');
   const srcDefault = img.getAttribute('src');
   const srcHover = srcDefault.replace('.png', '_color.png');
+
+  card.addEventListener('mouseenter', () => {
+    img.setAttribute('src', srcHover);
+  });
+
+  card.addEventListener('mouseleave', () => {
+    img.setAttribute('src', srcDefault);
+  });
+});
+
+const slider = document.querySelectorAll('.slider');
+
+slider.forEach(card => {
+  const img = card.querySelector('.slider img');
+  const srcDefault = img.getAttribute('src');
+  const srcHover = srcDefault.replace('_pb.svg', '.svg');
 
   card.addEventListener('mouseenter', () => {
     img.setAttribute('src', srcHover);
